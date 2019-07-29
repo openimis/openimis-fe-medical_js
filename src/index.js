37301a444274d6559fdd5f7da07e1a460d6b42d3
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-07-29 16:01:17 +0200

    intermediate 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React, { Component } from "react";
+import messages_en from "./translations/en.json";
+import reducer from "./reducer";
+import ItemSimpleSearcher from "./components/ItemSimpleSearcher";
+import ServiceSimpleSearcher from "./components/ServiceSimpleSearcher";
 
-class MedicalModule extends Component {
-  static menu = null;
-  static routes = [];
+const DEFAULT_CONFIG = {
+  "translations": [{ key: 'en', messages: messages_en }],
+  "reducers": [{ key: 'medical', reducer: reducer }],
+  "components": [
+    {key: "medical.ItemSimpleSearcher", component: ItemSimpleSearcher },
+    {key: "medical.ServiceSimpleSearcher", component: ServiceSimpleSearcher },
+  ],    
 }
 
-export default MedicalModule;
+// OTHER MODULE PARAMETERS:
+// - cacheItems
+// - cacheServices
+
+export const MedicalModule = (cfg) => {
+  return { ...DEFAULT_CONFIG, ...(cfg && cfg['fe-medical'] || {}) };
+}
---
+++ src/reducer.js
@@ -0,0 +1,66 @@
+import { formatServerError, formatGraphQLError } from '@openimis/fe-core';
+
+function reducer(
+    state = {
+        fetchingItems: false,
+        fetchedItems: false,
+        errorItems: null,
+        items: null,
+        fetchingServices: false,
+        fetchedServices: false,
+        errorServices: null,
+        services: null,
+    },
+    action,
+) {
+    switch (action.type) {
+        case 'MEDICAL_ITEMS_REQ':
+            return {
+                ...state,
+                fetchingItems: true,
+                fetchedItems: false,
+                items: null,
+                errorItems: null,
+            };
+        case 'MEDICAL_ITEMS_RESP':
+            return {
+                ...state,
+                fetchingItems: false,
+                fetchedItems: true,
+                items: action.payload.data.medicalItems,
+                errorItems: formatGraphQLError(action.payload)
+            };
+        case 'MEDICAL_ITEMS_ERR':
+            return {
+                ...state,
+                fetchingItems: false,
+                errorItems: formatServerError(action.payload)
+            };
+        case 'MEDICAL_SERVICES_REQ':
+            return {
+                ...state,
+                fetchingServices: true,
+                fetchedServices: false,
+                services: null,
+                errorServices: null,
+            };
+        case 'MEDICAL_SERVICES_RESP':
+            return {
+                ...state,
+                fetchingServices: false,
+                fetchedServices: true,
+                services: action.payload.data.medicalServices,
+                errorServices: formatGraphQLError(action.payload)
+            };
+        case 'MEDICAL_SERVICES_ERR':
+            return {
+                ...state,
+                fetchingServices: false,
+                errorServices: formatServerError(action.payload)
+            };
+        default:
+            return state;
+    }
+}
+
+export default reducer;
