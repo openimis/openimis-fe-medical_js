2daaf0ccde3d4ec0bf3da44d7cbb95eb3707d89f
Eric Darchis <darchis@pobox.com>
2021-10-28 15:40:12 +0200

    Poor merge in previous commit

--- src/pages/MedicalServiceOverviewPage.js
+++ src/pages/MedicalServiceOverviewPage.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { injectIntl } from "react-intl";
 import { connect } from "react-redux";
 import { Edit as EditIcon } from "@material-ui/icons";
-import { historyPush, withModulesManager, withHistory, ErrorBoundary, formatMessage } from "@openimis/fe-core";
 import {
   ErrorBoundary,
   formatMessageWithValues,
