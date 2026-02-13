03472a12071d4076664d01041bd313a2e2566df3
olewandowski1 <olewandowski@soldevelo.com>
2026-02-13 15:13:41 +0100

    fix: provide modules manager

--- src/components/MedicalItemSearcher.jsx
+++ src/components/MedicalItemSearcher.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { injectIntl } from "react-intl";
-import { IconButton, Tooltip } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import { Tab as TabIcon, Delete as DeleteIcon } from "@mui/icons-material";
 import {
   withModulesManager,
--- src/components/MedicalServiceSearcher.jsx
+++ src/components/MedicalServiceSearcher.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { injectIntl } from "react-intl";
-import { IconButton, Tooltip } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import { Tab as TabIcon, Delete as DeleteIcon } from "@mui/icons-material";
 import {
   withModulesManager,
@@ -109,9 +109,7 @@ class MedicalServiceSearcher extends Component {
   deleteAction = (i) => {
     return !!i.validityTo || !!i.clientMutationId ? null : (
       <Tooltip title={formatMessage(this.props.intl, "medical.service", "deleteService.tooltip")}>
-        <Button onClick={(e) => this.confirmDelete(i)}
-          startIcon={<DeleteIcon />}
-        >
+        <Button onClick={(e) => this.confirmDelete(i)} startIcon={<DeleteIcon />}>
           {formatMessage(this.props.intl, "medical.service", "deleteServiceButton.buttonText")}
         </Button>
       </Tooltip>
@@ -136,9 +134,7 @@ class MedicalServiceSearcher extends Component {
           : null,
       (ms) => (
         <Tooltip title={formatMessage(this.props.intl, "medical.service", "openNewTab")}>
-          <Button onClick={(e) => this.props.onDoubleClick(ms, true)}
-            startIcon={<TabIcon />}
-          >
+          <Button onClick={(e) => this.props.onDoubleClick(ms, true)} startIcon={<TabIcon />}>
             {formatMessage(this.props.intl, "medical.service", "openNewTabButton.buttonText")}
           </Button>
         </Tooltip>
